9a53ec1d60e5dd41a37604cd01bfb9e081e3eb10
Prescott Rowe <prescottrowe@gmail.com>
2020-02-27 20:07:51 -0800

    update

--- LAB2_ListView_Cells/LAB2_ListView_Cells/MainPage.xaml.cs
+++ LAB2_ListView_Cells/LAB2_ListView_Cells/MainPage.xaml.cs
@@ -11,29 +11,29 @@ namespace LAB2_ListView_Cells
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
-        public List<Meme> AllMemes { get; set; }
+        public List<contact> Allcontacts { get; set; }
         public MainPage()
         {
             InitializeComponent();
         }
-        public class Meme
+        public class contact
         {
-            public string MemeId { get; set; }
-            public string MemeName { get; set; }
+            public string contactId { get; set; }
+            public string contactName { get; set; }
             public string Url { get; set; }
         }
-        public class Memes
+        public class contacts
         {
-            public static IEnumerable<Meme> Get()
+            public static IEnumerable<contact> Get()
             {
-                return new List<Meme>
+                return new List<contact>
                 {
-                  new Meme() {MemeId="1", MemeName="Bart", Url="bart.png"},
-                  new Meme() {MemeId="2", MemeName="Bob", Url="bob.png" },
-                  new Meme() {MemeId="3", MemeName="Jerry", Url="jerry.png" },
-                  new Meme() {MemeId="4", MemeName="Puff", Url="puff.jpg" },
-                  new Meme() {MemeId="5", MemeName="Quinn", Url="quinn.jpg" },
-                  new Meme() {MemeId="6", MemeName="Reggie", Url="reggie.png"}
+                  new contact() {contactId="1", contactName="Bart", Url="bart.png"},
+                  new contact() {contactId="2", contactName="Bob", Url="bob.png" },
+                  new contact() {contactId="3", contactName="Jerry", Url="jerry.png" },
+                  new contact() {contactId="4", contactName="Puff", Url="puff.jpg" },
+                  new contact() {contactId="5", contactName="Quinn", Url="quinn.jpg" },
+                  new contact() {contactId="6", contactName="Reggie", Url="reggie.png"}
                 };
             }
         }
@@ -41,8 +41,8 @@ namespace LAB2_ListView_Cells
         protected override void OnAppearing()
         {
             base.OnAppearing();
-            AllMemes = new List<Meme>(Memes.Get());
-            ListViewList.ItemsSource = AllMemes;
+            Allcontacts = new List<contact>(contacts.Get());
+            ListViewList.ItemsSource = Allcontacts;
         }
     }
 }
